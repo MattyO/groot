f4326ffe43fc12f860fe78de078c011dc0e99b1f
Gary Johnson <gary@gjtt.com>
2014-06-13 11:27:33 -0400

    Incrementing version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='groot',
-    version='0.0.1',
+    version='0.0.2',
     author='Gary Johnson',
     author_email = 'gary@gjtt.com',
     description = 'Automation for BDD testing PyQt apps',
