d1d51fa22273dfb8330ff6dc38c9eda6311360e6
Gary Johnson <gary@gjtt.com>
2014-06-16 08:31:31 -0400

    Debugging

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='groot',
-    version='0.0.3',
+    version='0.0.4',
     author='Gary Johnson',
     author_email = 'gary@gjtt.com',
     description = 'Automation for BDD testing PyQt apps',
