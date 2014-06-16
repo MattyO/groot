d94212dd936be1b26782dfbc193222f1139bea14
Gary Johnson <gary@gjtt.com>
2014-06-16 08:12:02 -0400

    Ping fails if window has no root elements yet

--- groot.py
+++ groot.py
@@ -10,7 +10,7 @@ from PyQt5.QtWidgets import QApplication
 
 @bottle.get("/ping")
 def ping():
-    if len(QApplication.topLevelWidgets()) == 0
+    if len(QApplication.topLevelWidgets()) == 0:
         abort(503, "Still booting up, try again later")
 
     return 'Ping!'
